6d34e7e7053186e2f3a2bc0ecb0dfb28bac97e37
Michiel Schoofs <michiel.schoofs@student.hogent.be>
2019-01-05 16:23:13 +0100

    Creër klasse BankAccount

---
+++ BankingApp/Models/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BankingApp.Models {
+    public class BankAccount {
+        #region Fields
+        private string _accountNumber; 
+        #endregion
+
+        #region Properties
+        public string AccountNumber {
+            get { return _accountNumber; }
+            private set { _accountNumber = value; }
+        }
+
+        public decimal Balance { get; set; } = 0M;
+        #endregion
+
+        #region Constructors
+        public BankAccount(string accountNumber) {
+            AccountNumber = accountNumber;
+        }
+        #endregion
+
+        #region Methods
+        public void Deposit(decimal amount) {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount) {
+            Balance -= amount;
+        } 
+        #endregion
+    }
+}
--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -1,9 +1,18 @@
-﻿using System;
+﻿using BankingApp.Models;
+using System;
 
 namespace BankingApp {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            BankAccount myAccount = new BankAccount("123-123123-12");
+            Console.WriteLine($"AccountNumber: {myAccount.AccountNumber}");
+            Console.WriteLine($"Balance: {myAccount.Balance}");
+
+            myAccount.Deposit(200M);
+            Console.WriteLine($"Balance after depositing 200 euros:{myAccount.Balance}");
+            myAccount.Withdraw(100M);
+            Console.WriteLine($"Balance after withdrawing 100 euros:{myAccount.Balance}");
+            Console.ReadKey();
         }
     }
 }
